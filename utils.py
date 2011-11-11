eba033badd12adcc0bd00fa13a08a33a9f84cf63
Philip Whitfield <public@underdoeg.com>
2011-11-11 12:18:30 +0100

    more util functions

--- utils.py
+++ utils.py
@@ -2,19 +2,40 @@ from models import *
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 
-def getCategories(slug = "", id = -1):
-    ret =  Category.objects.filter(parent__isnull=True)
-    for c in ret:
-        if id != -1 and c.id == active:
-            c.active = True
-        if slug != "" and c.slug == slug:
+def getCategoriesRecursiveHelper(parent, activeId):
+    if parent == None:
+        set = Category.objects.filter(parent__isnull=True)
+    else:
+        set = Category.objects.filter(parent=parent.id)
+    
+    for c in set:
+        c.parent = parent;
+        if c.id == activeId:
+            p = parent
+            while p is not None:
+                p.active = True
+                p = p.parent
             c.active = True
         else:
             c.active = False
+        c.children = getCategoriesRecursiveHelper(c, activeId)
+    return set
+    
+def getCategories(slug = "", id = -1):
+    activeId = id
+    if slug != "":
+        activeId = getCategoryIdBySlug(slug)
+    ret = getCategoriesRecursiveHelper(None, activeId)
     return ret
     
 def getCategoryIdBySlug(catSlug):
     return get_object_or_404(Category, slug__iexact=catSlug).id
+
+def getCategory(id=-1, slug=""):
+    if slug != "":
+        return get_object_or_404(Category, slug__iexact=slug)
+    else:
+        return get_object_or_404(Category, pk=id)
 
 def getPosts(showHidden = False, excludeCategories = []):
     ret = Post.objects.all()
@@ -43,11 +64,20 @@ def getPost(postId):
 def getPages():
     return Page.objects.all().filter(visible = True)
 
+def getPagesByCategory(catId):
+    return getPages().filter(category=catId)    
+
+def getPagesByCategorySlug(slug):
+    return getPagesByCategory(getCategoryIdBySlug(slug))
+
 def getPageIdBySlug(slug):
     return get_object_or_404(Page, slug__iexact=slug).id
     
 def getPage(postId):
     return Page.objects.get(id=postId)
+
+def getPageBySlug(slug):
+    return getPage(getPageIdBySlug(slug))
 
 ##image stuff
 try:
