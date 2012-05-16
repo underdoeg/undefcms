638186d11e14f6e6fe48c58e46b3d3bd80c92c2f
Philip Whitfield <public@underdoeg.com>
2012-05-16 19:49:28 +0200

    bugfix, no error on file save without file

--- models.py
+++ models.py
@@ -93,37 +93,38 @@ class File(models.Model):
     extra = fields.JSONField(null=True, blank=True, editable = False)
     
     def save(self, *args, **kwargs):
-        mime = mimetypes.guess_type(self.file.path)[0]
-        if self.extra is None:
-            self.extra = {}
-        self.extra["mime"] = mime
-        
-        if self.type == "auto": #We don't know the mime type yet. try to guess it
-            if mime == "image/jpeg" or mime == "image/png" or mime == "image/gif":
-                self.type = filetypes["image"]
-            elif mime == "video/mp4":
-                self.type = filetypes["video"]
-                if self.extra.has_key("lastFile") is False or self.extra["lastFile"] != self.file.filename or settings.DEBUG:
-                    res = commands.getoutput("ffmpeg -i "+self.file.path)
-                    dimFind = re.findall(r'(\d+x\d+)', res)
-                    if len(dimFind) is not 0:
-                        dim = dimFind[0].split("x")
-                        width = dim[0]
-                        height = dim[1]
-                        self.extra["width"] = width
-                        self.extra["height"] = height
-                    '''
-                    #time to encode
-                    videoPath = settings.MEDIA_ROOT+"encodedVideos/"
-                    comOGG = "ffmpeg -i "+self.file.path+" -acodec libvorbis -ac 2 -ab 96k -ar 44100 -b 345k -s "+dimFind[0]+" "+videoPath+self.file.filename+".ogv"
-                    res = commands.getoutput(comOGG)
-                    self.extra["encodeMsg"] = res
-                    '''
-            elif mime == 'audio/mpeg':
-                self.type = filetypes["audio"]
-            else:
-                self.type = mime
-        self.extra["lastFile"] = self.file.filename
+        if self.file:
+            mime = mimetypes.guess_type(self.file.path)[0]
+            if self.extra is None:
+                self.extra = {}
+            self.extra["mime"] = mime
+            
+            if self.type == "auto": #We don't know the mime type yet. try to guess it
+                if mime == "image/jpeg" or mime == "image/png" or mime == "image/gif":
+                    self.type = filetypes["image"]
+                elif mime == "video/mp4":
+                    self.type = filetypes["video"]
+                    if self.extra.has_key("lastFile") is False or self.extra["lastFile"] != self.file.filename or settings.DEBUG:
+                        res = commands.getoutput("ffmpeg -i "+self.file.path)
+                        dimFind = re.findall(r'(\d+x\d+)', res)
+                        if len(dimFind) is not 0:
+                            dim = dimFind[0].split("x")
+                            width = dim[0]
+                            height = dim[1]
+                            self.extra["width"] = width
+                            self.extra["height"] = height
+                        '''
+                        #time to encode
+                        videoPath = settings.MEDIA_ROOT+"encodedVideos/"
+                        comOGG = "ffmpeg -i "+self.file.path+" -acodec libvorbis -ac 2 -ab 96k -ar 44100 -b 345k -s "+dimFind[0]+" "+videoPath+self.file.filename+".ogv"
+                        res = commands.getoutput(comOGG)
+                        self.extra["encodeMsg"] = res
+                        '''
+                elif mime == 'audio/mpeg':
+                    self.type = filetypes["audio"]
+                else:
+                    self.type = mime
+            self.extra["lastFile"] = self.file.filename
         super(File, self).save(*args, **kwargs)
     
     def __unicode__(self):
